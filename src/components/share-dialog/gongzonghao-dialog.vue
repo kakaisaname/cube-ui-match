dd8fb8166e395788b591c64ecfab13261f740d57
caoyi <1198560793@qq.com>
2019-01-21 17:27:49 +0800

    '分享'

--- src/components/share-dialog/gongzonghao-dialog.vue
+++ src/components/share-dialog/gongzonghao-dialog.vue
@@ -10,10 +10,14 @@
 </template>
 
 <script>
+import axios from 'axios'
+import { Message } from 'element-ui'
+import wx from 'weixin-js-sdk'       //引入wx sdk
 export default {
   name: 'gongzonghao-dialog',
   mounted () {
-    this.show() 
+    this.show(),
+    this.share()
   },
   methods: {
     show () {
@@ -23,7 +27,78 @@ export default {
     hide () {
       this.$refs.popup.hide()
       this.$emit('hide')
-    }
+      Message.error('您已取消分享')
+    },
+    share(){
+            //也不知道为什么，分享 必须要刷新下页面才行，所以做以下判断
+            let shan_share_fresh = sessionStorage.getItem('shan_share_fresh')
+            if (!shan_share_fresh) {
+                sessionStorage.setItem('shan_share_fresh',1)
+                location.reload()
+            }
+            //获取页面URL
+            let requestUrl = "http://www.hhfff.cn/api/getWxConfig"
+            axios.get(requestUrl)
+            .then((response) => {
+                //判断是否获取成功
+                let res = response.data
+                if (res.code == '000') { //获取微信配置成功
+                    let result = res.data
+                    //----------微信配置信息----------------------
+                    wx.config({
+                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来
+                        appId: result.appId, // 必填，公众号的唯一标识
+                        timestamp: result.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: result.nonceStr, // 必填，生成签名的随机串
+                        signature: result.signature,// 必填，签名
+                        jsApiList: [
+                            // "updateAppMessageShareData", //自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+                            // "updateTimelineShareData",    //自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+                            "onMenuShareTimeline",    //自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+                            "onMenuShareAppMessage",  //分享给朋友
+			            ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    });
+                    //---------微信处理验证成功或失败信息--------------
+                    //处理验证失败的信息
+                    wx.ready(function () {
+                        wx.onMenuShareTimeline({ 
+                            title: '小善答题', // 分享标题
+                            link: 'http://questions.hhfff.cn/shareurl', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            imgUrl: 'http://118.24.61.194:8089/share.jpg', // 分享图标
+                            success: function () {
+                            // 设置成功
+                                sessionStorage.removeItem('shan_share_fresh')
+                                console.log("分享到朋友圈成功返回的信息为:", res);
+                                Message.success('分享到朋友圈成功');
+                                setTimeout(() => {
+                                    //分享后跳转 
+                                    window.location.href = "http://questions.hhfff.cn/share/gongzonghaoPicture"
+                                }, 3000)
+                                //直接跳转到首页
+                            },
+                            // cancel: function () {  //微信不给你机会判断用户到底有没有分享
+                            //     alert(4)
+                            //     sessionStorage.removeItem('shan_share_fresh')
+                            //     Message.error('分享到朋友圈失败');
+                            //     setTimeout(() => {
+                            //         window.location.href = "http://questions.hhfff.cn/"
+                            //     }, 3000)
+                            // }
+                        });
+                    })
+                     wx.error(function (res) {
+                        console.log('验证失败返回的信息:', res);
+                     });
+                } else if (res.code == '003') { //失败
+                    Message.success('分享功能，系统正在维护中。。。')
+                    return false
+                }
+            })
+            .catch((error) => {
+                Message.success('分享功能，系统正在维护中。。。')
+                return false
+            })
+        }
   }
 }
 </script>
